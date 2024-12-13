2c26bceed9955694c89eb83decd9b013ffd0f7d2
Jeferson <jferson-araujo@hotmail.com>
2024-12-12 23:34:45 -0300

    Layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutRoute() {
+    return (
+      <main className="p-5">
+        <h1 className="text-4xl font-bold">About Us</h1>
+        <div className="mt-5">About Us And Our Awesome Applications</div>
+      </main>
+    );
+  }
